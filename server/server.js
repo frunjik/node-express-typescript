f810a213272625a0bb6ee17dd438bf06c064b9ed
frunjik <frunjik@gmail.com>
2024-06-15 14:56:34 +0200

    server.js

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// src/index.js
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+console.log(process.env); 
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
